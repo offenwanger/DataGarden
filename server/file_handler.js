76be06eaca6c1110214af84ecc7addb0b191ecd3
Anna Offenwanger <dev@offenwanger.ca>
2023-10-02 10:30:24 +0200

    improve error handling

--- server/file_handler.js
+++ server/file_handler.js
@@ -3,15 +3,29 @@ const fs = require('fs');
 const SCAP_FOLDER = __dirname + '/scaps/'
 
 async function writeScap(filename, contents) {
-    fs.writeFileSync(SCAP_FOLDER + filename, contents, err => err ? console.error(err) : null);
+    try {
+        fs.writeFileSync(SCAP_FOLDER + filename, contents, err => err ? console.error(err) : null);
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 async function readScap(filename) {
-    return fs.readFileSync(SCAP_FOLDER + filename, 'utf8');
+    try {
+        return fs.readFileSync(SCAP_FOLDER + filename, 'utf8');
+    } catch (e) {
+        console.error(e);
+        return "";
+    }
 }
 
 async function deleteScap(filename) {
-    return fs.unlinkSync(SCAP_FOLDER + filename, 'utf8');
+    try {
+        return fs.unlinkSync(SCAP_FOLDER + filename, 'utf8');
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
 }
 
 module.exports = {
