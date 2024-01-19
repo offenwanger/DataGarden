ecc07f79c64ebb986f4f61169c6c289b2655e26c
Anna Offenwanger <dev@offenwanger.ca>
2024-01-19 09:07:23 +0100

    fix another dumb bug

--- local/js/utils/structure_fairy.js
+++ local/js/utils/structure_fairy.js
@@ -78,6 +78,7 @@ export let StructureFairy = function () {
 
         let elements = model.getElements()
             .filter(e => dimension.tier == DataUtil.getTier(model, e.id));
+        if (elements.length == 0) return;
         let levels = dimension.levels;
 
 
