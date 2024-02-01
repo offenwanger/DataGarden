01de4bbc65bbea6314cc33f11eb37be9f3a5a490
Anna Offenwanger <dev@offenwanger.ca>
2024-02-01 12:57:57 +0100

    remove unimplimented tests

--- local-test/fdl_view_controller_test.js
+++ local-test/fdl_view_controller_test.js
@@ -19,54 +19,6 @@ describe('FDL View Controller Test', function () {
         integrationEnv.cleanup(done);
     });
 
-    describe('element parenting tests', function () {
-        it('should parent two elements', function () {
-            assert.equal('implimented', true)
-        });
-
-        it('should parent groups of elements', function () {
-            assert.equal('implimented', true)
-        });
-
-        it('should prevent parenting loops', function () {
-            assert.equal('implimented', true)
-        });
-
-        it('should parent elements via the bubble', function () {
-            assert.equal('implimented', true)
-        });
-
-        it('should reparent on delete', function () {
-            assert.equal('implimented', true)
-        });
-    })
-
-    describe('deletion tests', function () {
-        it('should delete a dimension', function () {
-            assert.equal('implimented', true)
-        });
-
-        it('should delete a category', function () {
-            assert.equal('implimented', true)
-        });
-
-        it('should delete a stroke', function () {
-            assert.equal('implimented', true)
-        });
-
-        it('should delete an element', function () {
-            assert.equal('implimented', true)
-        });
-
-        it('should delete a selection of everything', function () {
-            assert.equal('implimented', true)
-        });
-
-        it('should only delete categories and dimensions when on an appropriate tab', function () {
-            assert.equal('implimented', true)
-        });
-    })
-
     describe('dimension setting tests', function () {
         it('should create a dimension', function () {
             utility.clickTab(Tab.LEGEND);
